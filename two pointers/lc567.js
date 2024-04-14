f6425ae740cafb4b771c3574931cdb3195d7a657
samsslee <150728297+samsslee@users.noreply.github.com>
2024-04-14 10:52:47 -0700

    Update lc567.js

--- two pointers/lc567.js
+++ two pointers/lc567.js
@@ -1,35 +1,44 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
 var checkInclusion = function(s1, s2) {
     let s1len = s1.length
     let s2len = s2.length
     
-    if (s1len > s2len) { return false }
+    //if s1 is larger than s2, then it won't work
+    if (s1len > s2len) {return false}
 
     let charMap = {}
     let lp = 0
     let rp = 0
     let countS1Chars = 0
 
+    //setup map
     for (let i = 0; i < s1len; i++){
         let curr = charMap[s1[i]] ? charMap[s1[i]] : 0
-        charMap[s1[i]] = curr + 1
+        charMap[s1[i]] = curr+1
     }
 
-    while (rp < s2len) {
-        if (charMap[s2[rp]] !== undefined) {
+    //main loop
+    while (rp < s2len){
+        //expand window
+        if (charMap[s2[rp]] !== undefined){
             charMap[s2[rp]]--
-            if (charMap[s2[rp]] >= 0) {
+            if(charMap[s2[rp]]>=0){
                 countS1Chars++
             }
         }
-
-        if (rp - lp + 1 === s1len) {
-            if (countS1Chars === s1len) {
+        
+        while(countS1Chars == s1len){
+            if(rp-lp+1 == s1len){
                 return true
             }
 
-            if (charMap[s2[lp]] !== undefined) {
+            if (charMap[s2[lp]] !== undefined){
                 charMap[s2[lp]]++
-                if (charMap[s2[lp]] > 0) {
+                if(charMap[s2[lp]]>0){
                     countS1Chars--
                 }
             }
@@ -40,4 +49,5 @@ var checkInclusion = function(s1, s2) {
         rp++
     }
     return false
+
 };
