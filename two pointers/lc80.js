c34fbbaeed6d7dd1e3346f6a911b1d6dfafd2f62
samsslee <150728297+samsslee@users.noreply.github.com>
2024-04-16 17:14:03 -0700

    Update lc80.js to make it more parallel in structure to 26

--- two pointers/lc80.js
+++ two pointers/lc80.js
@@ -13,15 +13,15 @@ var removeDuplicates = function(nums) {
     let c = 0
 
     while (rp<n){
-        if(nums[lp]!=nums[rp]){
-            lp++
-            nums[lp] = nums[rp]
-            c=0
-        } else if(nums[lp] == nums[rp] && c ==0){
+        if(nums[lp] == nums[rp] && c ==0){
             c++
             lp++
             nums[lp] = nums[rp] //this is to capture the case where you're at the end and lp+1 isn't the same as the largest number but lp is
-        }
+        } else if(nums[lp]!=nums[rp]){
+            lp++
+            nums[lp] = nums[rp]
+            c=0
+        } 
         rp++
     }
     return lp+1
